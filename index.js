ac49c806b71add364d5622fedb115cab512f1781
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-02-22 00:13:13 -0300

    Atualização
    biblioteca body-parser

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 const express = require('express')
 const app = express()
-const handlebars =  require('express-handlebars')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 //Configurar o handlebars
 // Template engine
-app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
+//configurar o body-parser
+//Utilizado para receber os valores do formulário método
+//post
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//===========================================
 
 
 //Conexão com o banco de dados mariadb
-
-const sequelize = new Sequelize('test','root','',{
-    host:"localhost",
-    dialect:'mariadb'
+const sequelize = new Sequelize('test', 'root', '', {
+    host: "localhost",
+    dialect: 'mariadb'
 })
-sequelize.authenticate(function(){
+sequelize.authenticate(function () {
     console.log("Conexao ok")
-}).catch(function(erro){
+}).catch(function (erro) {
     console.log("Falha ao conectar" + erro)
 })
 
 //=========================================================
 
-app.get("/cad",function(req, res){
-    res.render('layouts/formulario')
-    
+app.get("/",function(req, res){
+    res.render('layouts/main')
+ 
     })
-    
+app.get("/cad", function (req, res) {
+    res.render('layouts/formulario')
 
-    app.post("/salvar",function(req, res){
-        res.send('salvar dados aqui')
-        
-        })
+})
+app.post("/salvar", function (req, res) {
+   
+   
+    res.send("Titulo: " + req.body.titulo + " Texto: " + req.body.conteudo)
 
-
+})
 
 app.listen(8081, function () {
     console.log("servidor rodando")
